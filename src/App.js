b4b826866ca9ac4d6a31020aae75313863220718
mukundc-iprogrammer <mukundc@iprogrammer.com>
2021-03-19 13:51:18 +0530

    Base feature and syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>I'm a react app</h1>
-    </div>
-  );
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+
+  state = {
+    username: 'supermax'
+  }
+
+  usernameChangeHandler = (event) => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>I'm a react app</h1>
+        <UserInput 
+          changed={this.usernameChangeHandler}
+          currentName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName={this.state.username} />
+        <UserOutput userName="Max" />
+      </div>
+    );
+  }
 }
 
 export default App;
